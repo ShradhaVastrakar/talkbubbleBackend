6f74b4b38136a788918ce83804166e1eb4cdd2c3
ShradhaVastrakar <shradhavastrakar@gmail.com>
2023-06-20 20:20:47 +0530

    envt variable

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", () => {
+  console.log("Welcome to the Home page of Talkbubble")
+})
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
